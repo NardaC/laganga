1a283d07bdb86874b47a33a055b3e13d57bacb08
NardaC <nardac6@gmail.com>
2020-10-02 11:10:10 -0500

    tags offers

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -9,7 +9,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { faClock } from "@fortawesome/free-regular-svg-icons";
 
-const Item = ({ product, addInterest}) => {
+const Item = ({ product, addInterest }) => {
   const [text, setText] = useState("");
   const [dataNueva, setDataNueva] = useState([]);
   const [dataOriginal, setDataOriginal] = useState([]);
@@ -19,22 +19,22 @@ const Item = ({ product, addInterest}) => {
   const [timeMinutes, setTimeMinutes] = useState(0);
   const [agotadoProduct, setAgotadoProduct] = useState("");
   const [timeSeconds, setTimeSeconds] = useState(0);
-  const dateFuture = "10/10/2020"
+  const dateFuture = "10/10/2020";
   React.useEffect(() => {
     fetch("data/data.json")
       .then((response) => response.json())
       .then((datos) => {
         setDataOriginal(datos);
       });
-      calculateTimeLeft(product.fechaFinOferta)
+    calculateTimeLeft(product.fechaFinOferta);
     const timer = setInterval(() => {
-        calculateTimeLeft(product.fechaFinOferta);
+      calculateTimeLeft(product.fechaFinOferta);
     }, 1000);
     return () => clearTimeout(timer);
   }, []);
 
   const calculateTimeLeft = (dateFuture) => {
-    // let year = new Date().getFullYear(); 
+    // let year = new Date().getFullYear();
     const difference = +new Date(dateFuture) - +new Date();
     if (difference > 0) {
       setTimeDays(Math.floor(difference / (1000 * 60 * 60 * 24)));
@@ -42,23 +42,47 @@ const Item = ({ product, addInterest}) => {
       setTimeMinutes(Math.floor((difference / 1000 / 60) % 60));
       setTimeSeconds(Math.floor((difference / 1000) % 60));
     } else {
-      setAgotadoProduct("Promoción Agotada")
+      setAgotadoProduct("Promoción Agotada");
     }
   };
 
   return (
     <div className="col-12 col-sm-4 col-lg-3 mb-4">
       <Card className={"border-" + product.categoria}>
-        <div className="tag-offer">-{product.descuento}%</div>
+        {/* <div className="tag-offer">
+          -{product.descuento}%
+        </div> */}
+
+
+        <div className="box-tag-offer">
+          <div className="box-free-shipping">
+            <div className="letter-free">free</div>
+            <div className="box-letter-free">shipping</div>
+          </div>
+        </div>
+
+
+        {/* <div className="box-tag-offer">
+          <div className="box-x">
+            3x2
+          </div>
+        </div> */}
+
+
+        {/* <div className="box-tag-offer">
+          <div className="box-porcent">
+            -25%
+          </div>
+        </div> */}
+
+
+
         <div className="group-btn-like">
-          {/* <a href="https://react-bootstrap.netlify.app/components/alerts/#additional-content">
-            <FontAwesomeIcon icon={faShareAlt} className="btn-share" />
-          </a> */}
           <a>
             <FontAwesomeIcon
               icon={faHeart}
-              className={product.like? "btn-like-active": "btn-like"}
-              onClick={()=>addInterest(product)}
+              className={product.like ? "btn-like-active" : "btn-like"}
+              onClick={() => addInterest(product)}
             />
           </a>
         </div>
@@ -68,13 +92,16 @@ const Item = ({ product, addInterest}) => {
         <a href={"/item-especific/" + product._id} className="link">
           <div className="group-time-item">
             <FontAwesomeIcon icon={faClock} />
-            {
-               agotadoProduct==="" ? (<div className="ml-2 item-chronometer">{timeDays}D {timeHours}H  {timeMinutes}M</div> ):(<div className="ml-2 item-chronometer">{agotadoProduct}</div>)
-            }
-            
+            {agotadoProduct === "" ? (
+              <div className="ml-2 item-chronometer">
+                {timeDays}D {timeHours}H {timeMinutes}M
+              </div>
+            ) : (
+              <div className="ml-2 item-chronometer">{agotadoProduct}</div>
+            )}
           </div>
           <Card.Body>
-        <Card.Title className="title-item">{product.nombre}</Card.Title>
+            <Card.Title className="title-item">{product.nombre}</Card.Title>
             <Card.Text className="subtitle-item">
               {product.descripcion}
             </Card.Text>
