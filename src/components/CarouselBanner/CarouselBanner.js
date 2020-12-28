33a81a509ae6d8e47dd9bdd2899119c0d054f3b2
Judith Cristina Quiñones Inga <judicriss18@gmail.com>
2020-12-28 18:32:05 -0500

    banner dinámico

--- src/components/CarouselBanner/CarouselBanner.js
+++ src/components/CarouselBanner/CarouselBanner.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 
 import Carousel from "react-bootstrap/Carousel";
 import banner1M from "../../images/banner/banner1-mobile.png";
@@ -7,21 +7,47 @@ import banner3M from "../../images/banner/banner3-mobile.png";
 import banner1D from "../../images/banner/banner1-desktop.png";
 import banner2D from "../../images/banner/banner2-desktop.png";
 import banner3D from "../../images/banner/banner3-desktop.png";
+import clienteAxiosBusinessLocal from "../../components/config/axiosBusinessLocal";
 
 const CarouselBanner = () => {
   const width = window.innerWidth;
+  const [bannerImage,setBannerImage] = useState([])
   const breakpoint = 768;
+  useEffect (()=>{
+    getBanner()
+
+  },[])
+  const getBanner = async () => {
+    await clienteAxiosBusinessLocal
+      .get("/banner/get-all/user")
+      .then((res) => {
+        if (res.data.MensajeRespuesta === "NO EXISTEN DATOS") {
+          setBannerImage([]);
+        } else {
+          setBannerImage(res.data);
+          console.log(res.data, "banner");
+        }
+      })
+      .catch((e) => {
+        console.log(e, "error:)");
+      });
+  };
   return (
     <>
       <Carousel>
-        <Carousel.Item>
-          {width < breakpoint ? (
-            <img className="d-block w-100" src={banner1M} alt="First slide" />
-          ) : (
-            <img className="d-block w-100" src={banner1D} alt="First slide" />
-          )}
-        </Carousel.Item>
-        <Carousel.Item>
+        {
+          bannerImage.map(item => (
+            <Carousel.Item>
+            {width < breakpoint ? (
+              <img className="d-block w-100" src={item.imagen[0].typeImage==="BM" ? item.imagen[0].url : item.imagen[1].url } alt="First slide" />
+            ) : (
+              <img className="d-block w-100" src={item.imagen[0].typeImage==="BD" ? item.imagen[0].url : item.imagen[1].url } alt="First slide" />
+            )}
+          </Carousel.Item>
+          ))
+        }
+      
+        {/* <Carousel.Item>
           {width < breakpoint ? (
             <img className="d-block w-100" src={banner2M} alt="Second slide" />
           ) : (
@@ -34,7 +60,7 @@ const CarouselBanner = () => {
           ) : (
             <img className="d-block w-100" src={banner3D} alt="Third slide" />
           )}
-        </Carousel.Item>
+        </Carousel.Item> */}
       </Carousel>
     </>
   );
